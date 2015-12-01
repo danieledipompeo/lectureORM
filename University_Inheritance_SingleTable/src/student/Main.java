61c456e1c292e366ec8aacdfc0ddf741af239138
daniele di pompeo <dipompeodaniele@gmail.com>
2015-12-01 18:23:30 +0100

    inheritance single table

---
+++ University_Inheritance_SingleTable/src/student/Main.java
@@ -0,0 +1,66 @@
+package student;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import util.HibernateUtil;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = null;
+		
+		Address address = new Address("OMR Road", "Chennai", "TN", "600097");
+		
+		try {
+			transaction = session.beginTransaction();
+			
+			Person person = new Person();
+			person.setName("Mike");
+			session.save(person);
+			
+			Transcript transcript1 = new Transcript("swr12");
+//Student(String studentName, Transcript transcript, Set<Phone> studentPhoneNumbers, Address studentAddress,Set<Course> courses)
+			Student student1 = new Student("Eswar", transcript1, null, null, null);
+			Transcript transcript2 = new Transcript("dnl21");
+			Student student2 = new Student("Daniel", transcript2, null, null, null);
+			session.save(student1);
+			session.save(student2);
+			
+			Set<Course> courses = new HashSet<Course>();
+			courses.add(new Course("Maths"));
+			courses.add(new Course("Computer Science"));
+
+			Criteria criteria = session.createCriteria(Student.class);
+			List<Student> students = (List<Student>) criteria.list();
+			
+			for(Student student : students){
+				Set<Phone> phoneNumbers = new HashSet<Phone>();
+				Phone house = new Phone("house","32354353");
+				Phone mobile = new Phone("mobile","32354353");
+				phoneNumbers.add(house);
+				phoneNumbers.add(mobile);
+				student.setStudentPhoneNumbers(phoneNumbers);
+				student.setCourses(courses);
+				student.setPersonAddress(address);
+				session.save(student);
+			}
+
+			transaction.commit();
+		} catch (HibernateException e) {
+			transaction.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+
+	}
+}
---
+++ University_Inheritance_SingleTable/src/student/Student.java
@@ -0,0 +1,67 @@
+package student;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+@DiscriminatorValue("Student")
+public class Student extends Person{
+
+	@OneToOne(fetch = FetchType.LAZY, cascade = { CascadeType.ALL })
+	@JoinColumn(name = "TranscriptID")
+	private Transcript transcript;
+
+	@OneToMany(fetch = FetchType.LAZY, cascade = { CascadeType.ALL })
+	private Set<Phone> studentPhoneNumbers = new HashSet<Phone>(0);
+
+	@ManyToMany(targetEntity = Course.class, cascade = { CascadeType.ALL })
+	@JoinTable(name = "Students_Courses", joinColumns = @JoinColumn(name = "Student_ID") , inverseJoinColumns = @JoinColumn(name = "Course_ID") )
+	private Set<Course> courses = new HashSet<Course>(0);
+
+	public Student() {
+	}
+
+	public Student(String studentName, Transcript transcript, Set<Phone> studentPhoneNumbers, Address studentAddress,
+			Set<Course> courses) {
+		this.name = studentName;
+		this.transcript = transcript;
+		this.studentPhoneNumbers = studentPhoneNumbers;
+		this.setPersonAddress(studentAddress);
+		this.courses = courses;
+	}
+
+	public Transcript getTranscript() {
+		return this.transcript;
+	}
+
+	public void setTranscript(Transcript transcript) {
+		this.transcript = transcript;
+	}
+
+	public Set<Phone> getStudentPhoneNumbers() {
+		return this.studentPhoneNumbers;
+	}
+
+	public void setStudentPhoneNumbers(Set<Phone> studentPhoneNumbers) {
+		this.studentPhoneNumbers = studentPhoneNumbers;
+	}
+
+	public Set<Course> getCourses() {
+		return this.courses;
+	}
+
+	public void setCourses(Set<Course> courses) {
+		this.courses = courses;
+	}
+}
