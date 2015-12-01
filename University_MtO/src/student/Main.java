82da50c4050dfaf92e07bae7382bd6e0546fcbbb
daniele di pompeo <dipompeodaniele@gmail.com>
2015-12-01 18:23:13 +0100

    many to one

---
+++ University_MtO/src/student/Main.java
@@ -0,0 +1,38 @@
+package student;
+
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import util.HibernateUtil;
+
+public class Main {
+
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			
+			Criteria criteria = session.createCriteria(Student.class);
+			List<Student> students = (List<Student>) criteria.list();
+			Address address = new Address("OMR Road", "Chennai", "TN", "600097");
+			for(Student student : students){
+				student.setStudentAddress(address);
+				session.save(student);
+			}
+			
+			transaction.commit();
+		} catch (HibernateException e) {
+			transaction.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+
+	}
+
+}
---
+++ University_MtO/src/student/Student.java
@@ -0,0 +1,91 @@
+package student;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Students")
+public class Student {
+
+	@Id
+	@Column(name="ID")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long studentId;
+	
+	@Column(name="Name")
+	private String studentName;
+	
+	@OneToOne(fetch=FetchType.LAZY, cascade = {CascadeType.ALL})
+	@JoinColumn(name="TranscriptID")
+	private Transcript transcript;
+
+	@OneToMany(fetch=FetchType.LAZY, cascade = {CascadeType.ALL})
+	private Set<Phone> studentPhoneNumbers = new HashSet<Phone>(0);
+	
+	@ManyToOne(fetch=FetchType.LAZY, cascade = {CascadeType.ALL})
+	@JoinColumn(name="AddressID")
+	private Address studentAddress;
+
+	public Student() {}
+
+	public Student(String studentName, Transcript transcript, Set<Phone> studentPhoneNumbers, Address studentAddress) {
+		this.studentName = studentName;
+		this.transcript = transcript;
+		this.studentPhoneNumbers = studentPhoneNumbers;
+		this.studentAddress = studentAddress;
+	}
+
+	public long getStudentId() {
+		return this.studentId;
+	}
+
+	public void setStudentId(long studentId) {
+		this.studentId = studentId;
+	}
+	
+	public String getStudentName() {
+		return this.studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	
+	public Transcript getTranscript(){
+		return this.transcript;
+	}
+	
+	public void setTranscript(Transcript transcript){
+		this.transcript = transcript;
+	}
+	
+	public Set<Phone> getStudentPhoneNumbers() {
+		return this.studentPhoneNumbers;
+	}
+
+	public void setStudentPhoneNumbers(Set<Phone> studentPhoneNumbers) {
+		this.studentPhoneNumbers = studentPhoneNumbers;
+	}
+	
+	public Address getStudentAddress() {
+		return this.studentAddress;
+	}
+
+	public void setStudentAddress(Address studentAddress) {
+		this.studentAddress = studentAddress;
+	}
+}
+
