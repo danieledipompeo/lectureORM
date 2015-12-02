59512444573fa1c57674f9359b9b6877841ee678
daniele di pompeo <dipompeodaniele@gmail.com>
2015-12-02 17:42:06 +0100

    inheritance Table per Class

---
+++ University_Inheritance_TablePerClass/src/student/Person.java
@@ -0,0 +1,56 @@
+package student;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
+@Entity
+public class Person {
+
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	protected long personId;
+
+	@Column(name = "Name")
+	protected String name;
+	
+	@ManyToOne(fetch = FetchType.LAZY, cascade = { CascadeType.ALL })
+	@JoinColumn(name = "AddressID")
+	protected Address personAddress;
+	
+	public Person(){}
+	
+	public long getPersonId() {
+		return this.personId;
+	}
+
+	public void setPersonId(long personId) {
+		this.personId = personId;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String studentName) {
+		this.name = studentName;
+	}
+
+	public Address getPersonAddress() {
+		return personAddress;
+	}
+
+	public void setPersonAddress(Address personAddress) {
+		this.personAddress = personAddress;
+	}
+}
---
+++ University_Inheritance_TablePerClass/src/student/Transcript.java
@@ -0,0 +1,45 @@
+package student;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Transcripts")
+public class Transcript {
+	
+	@Id
+	@Column(name="ID")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long transcriptId;
+	
+	@Column(name="IdNumber")
+	private String idNumber;
+	
+	public Transcript(){}
+	
+	public Transcript(String idNumber){
+		this.idNumber = idNumber;
+	}
+
+	public Long getTranscriptId() {
+		return transcriptId;
+	}
+
+	public void setTranscriptId(Long transcriptId) {
+		this.transcriptId = transcriptId;
+	}
+
+	public String getIdNumber() {
+		return idNumber;
+	}
+
+	public void setIdNumber(String idNumber) {
+		this.idNumber = idNumber;
+	}
+
+	
+}
